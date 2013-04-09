db80791c517bfb1cd1ddf80da77c3085a45cc5a9
root <root@box004.unimatrix.fr>
2013-04-10 00:37:20 +0200

    Add log history view to admin

--- inscriptions/admin.py
+++ inscriptions/admin.py
@@ -94,6 +94,35 @@ class EquipeAdminMini(admin.ModelAdmin):
     search_fields = ('id', 'nom', 'club', 'gerant_nom', 'gerant_prenom', 'equipier__nom', 'equipier__prenom')
 
 
+from django.contrib.admin.models import LogEntry
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+class LogAdmin(admin.ModelAdmin):
+    """Create an admin view of the history/log table"""
+    list_display = ('action_time','user','content_type','modified_object','change_message','is_addition','is_change','is_deletion')
+    list_filter = ['action_time','user','content_type']
+    ordering = ('-action_time',)
+    readonly_fields = [ 'user','content_type','object_id','object_repr','action_flag','change_message']
+    #We don't want people changing this historical record:
+    def has_add_permission(self, request):
+        return False
+    def has_change_permission(self, request, obj=None):
+        #returning false causes table to not show up in admin page :-(
+        #I guess we have to allow changing for now
+        return True
+    def has_delete_permission(self, request, obj=None):
+        return False
+    def modified_object(self, obj=None):
+        if not obj:
+            return ''
+        return mark_safe(u'<a href="/admin/%s">%s</a>' % (
+            obj.get_admin_url(),
+            escape(obj.object_repr)
+        ))
+    modified_object.allow_tags = True
+admin.site.register(LogEntry, LogAdmin)
+
+
 admin.site.disable_action('delete_selected')
 #admin.site.register(Equipe, EquipeAdmin)
 admin.site.register(Equipe, EquipeAdminMini)
