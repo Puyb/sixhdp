3a9ecf307e91c69de046b50c715a4e11a34eb693
root <root@box004.unimatrix.fr>
2013-06-26 22:43:15 +0200

    .

--- inscriptions/static/admin.js
+++ inscriptions/static/admin.js
@@ -16,21 +16,11 @@ Event.observe(window, 'load', function() {
 
     $$('.add-row, .delete').invoke('remove');
 
-    $$('.field-date_de_naissance p').each(function(p) {
-        var d = p.textContent.split(' ');
-        console.log(d);
-        d[1] = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'].indexOf(d[1]);
-        d = d.collect(parseFloat);
-        console.log(d);
-        var date_de_naissance= new Date(d[2], d[1], d[0]);
+    $$('.field-age>p').each(function(p) {
 
-        today = new Date(YEAR, MONTH - 1, DAY)
+        console.log(p.textContent)
+        if(parseFloat(p.textContent) >= 18) {
 
-        birthday = new Date(YEAR, d[1], d[0]);
-        if(birthday.getDay() != d[0])
-            birthday = new Date(YEAR, d[1], d[0] - 1);
-
-        if(parseFloat($$('.field-age>p')[0].textContent) > 18) {
             p.up('.form-row').next().hide();
         }
     
